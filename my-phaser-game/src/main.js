50f4b7d5d4ba72f98db406f14ee3569e1ef4e180
gemy3731 <mohamedgamalz3731@gmail.com>
2024-12-06 18:49:33 +0200

    feat: create new project

---
+++ my-phaser-game/src/main.js
@@ -0,0 +1,11 @@
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+
+};
+
+export default new Phaser.Game(config);
